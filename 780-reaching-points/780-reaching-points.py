75f28307e6182a55d71f5f6a70bfc677a85b189e
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2022-01-30 03:41:52 +0800

    Time: 54 ms (17.08%), Space: 14 MB (98.29%) - LeetHub

--- 780-reaching-points/780-reaching-points.py
+++ 780-reaching-points/780-reaching-points.py
@@ -1,36 +1,27 @@
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
         
-        if tx<sx or ty<sy:
+        if sx> tx or sy >ty:
             return False
         
-        if tx ==sx:
-            
-            return (ty-sy)%sx == 0
-        
-        if ty == sy:
-            
-            return (tx-sx)%sy == 0
+        if sx == tx:
+            return (ty-sy) % sx == 0
+        elif sy ==ty:
+            return (tx-sx)% sy == 0 
         
         while tx>sx and ty>sy:
             
             if tx>ty:
                 tx = tx%ty
-                
-            elif ty>=tx:
+            else:
                 ty = ty%tx
                 
-                
-        if tx ==sx:
-            
-            return (ty-sy)%sx == 0
-        
-        if ty == sy:
-            
-            return (tx-sx)%sy == 0
+        if sx == tx:
+            return (ty-sy) % sx == 0 
+        elif sy ==ty:
+            return (tx-sx)% sy == 0
         
         return False
-                
         
         
         
