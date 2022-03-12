bc73e0d4c96d75eec46bd8897932251f77293b6a
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2022-03-12 19:18:54 +0800

    Time: 310 ms (5.02%), Space: 20.2 MB (5.74%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -0,0 +1,77 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = deque()
+        
+        s= re.sub(r"\s+", "", s)
+        index = 0 
+        
+        while index<len(s):
+            
+            
+            if s[index] == '/' :
+                num = int(stack.pop())
+                index+=1
+                temp =""
+                while index<len(s) and s[index].isdigit():
+                    
+                    temp += s[index]
+                    index+=1
+                cal = floor(num/int(temp))
+                stack.append(str(cal))
+                
+                
+            elif s[index] =='*':
+                num = int(stack.pop())
+                index+=1 
+                temp = ""
+                while index<len(s) and s[index].isdigit():
+                    
+                    temp += s[index]
+                    index+=1
+                cal = num * (int(temp))
+                stack.append(str(cal))
+                
+                
+            
+            else:
+                temp = ''
+                while index<len(s) and s[index].isdigit():
+                    temp += s[index]
+                    index+=1 
+                if temp =="":
+                    temp += s[index]
+                    index+=1 
+            
+                stack.append(temp)
+                
+                
+        print(stack)       
+        #print(int(0))
+        ans = 0 
+        neg =False
+        while stack:
+            
+            ele = stack.popleft()
+            #print(ele)
+            
+            if ele =='-':
+                neg =True
+                continue
+            if ele =='+':
+                continue
+            
+            elif neg:
+                ans-=int(ele)
+                
+                neg=False
+            else:
+                ans += int(ele)
+                
+        return ans
+                
+            
+            
+            
+            
+            
+            
