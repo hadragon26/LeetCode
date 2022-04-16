dd406778406da694e13dd314ef2db064cf669627
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2022-04-17 00:30:43 +0800

    Time: 652 ms (87.13%), Space: 16.2 MB (75.40%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -5,7 +5,7 @@ class Solution:
         dp = [[0 for i in range(len(matrix[0])+1)] for i in range(len(matrix)+1)]
         
         
-        
+        su = 0
         for i in range(1,len(matrix)+1):
             for c in range(1,len(matrix[0])+1):
                 
@@ -14,13 +14,10 @@ class Solution:
                 if matrix[i-1][c-1] == 1:
                     dp[i][c] = min(dp[i][c-1],dp[i-1][c],dp[i-1][c-1])+1
                     
-                    
-        
-        su = 0
-        
-        for i in range(1,len(matrix)+1):
-            for c in range(1,len(matrix[0])+1):
-                
                 su += dp[i][c]
+        
+        
+        
+        
                 
         return su
