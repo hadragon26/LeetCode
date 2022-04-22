9af29125140a45f638667fe6637d7228fe363ad6
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2022-04-22 16:08:01 +0800

    Time: 1095 ms (10.63%), Space: 125.9 MB (6.41%) - LeetHub

--- 877-stone-game/877-stone-game.py
+++ 877-stone-game/877-stone-game.py
@@ -1,7 +1,39 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         
-        return True
+        dic = {}
+        
+        
+        def dfs(l,r):
+            if l>r:
+                return 0
+            if (l,r) in dic:
+                return dic[(l,r)]
+            
+            
+            if (r-l) % 2 != 0:
+                even =True
+            else:
+                even =False
+                
+            if even:
+                left = piles[l]
+                right = piles[r]
+            
+            else:
+                left = 0 
+                right = 0 
+                
+            
+            
+            dic[(l,r)] = max(dfs(l+1,r)+left,dfs(l,r-1)+right)
+            
+            return dic[(l,r)]
+        
+        return(dfs(0,len(piles)-1)) > sum(piles)//2
+    
                     
+            
+            
                     
         
